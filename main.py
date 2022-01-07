2b8df95e2ce33b2fa9e5eb9ea910ab3cb4b65070
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2022-01-07 14:06:15 -0500

    comment input validator & return validated input

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import random, numpy, math, matplotlib.pyplot as plt
 
 
+def validate_tsp_input(pts):
+    """ Validates list of points passed as input to solve_tsp.
+        Ensures:
+            - There are at least 2 points
+            - All points have the same dimension
+            - Dimension of the points is at least 1
+
+        Returns a tuple containing the list of points, the number of points, and the dimension of the points.
+    """
+    n = len(pts)
+    assert n >= 2
+
+    d = len(pts[0])
+    assert d >= 1
+    for i in range(n):
+        assert d == len(pts[i])
+
+    return pts, n, d
+
+
 # Adapted from https://ericphanson.com/blog/2016/the-traveling-salesman-and-10-lines-of-python/
 
-def solve_tsp(pts):
-    validate_tsp_input(pts)
-    n = len(pts)
+def solve_tsp(input_points):
+    pts, n, d = validate_tsp_input(input_points)
     tour = list(range(0, n))
 
     for temp in numpy.logspace(0, 5, num=100000)[::-1]:
         # Create a new tour by randomly swapping 2 points in the previous tour
-        [i, j] = sorted(random.sample(range(n), 2));
+        [i, j] = sorted(random.sample(range(n), 2))
         new_tour = tour.copy()
         new_tour[j] = tour[i]
         new_tour[i] = tour[j]
@@ -22,15 +41,6 @@ def solve_tsp(pts):
             tour = new_tour.copy()
 
     return tour
-
-
-def validate_tsp_input(pts):
-    n = len(pts)
-    assert n > 1
-
-    d = len(pts[0])
-    for i in range(n):
-        assert d == len(pts[i])
 
 
 def dist(p1, p2):

3e25b1cb75088c357c246047fd98924d16148e82
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2022-01-07 13:53:04 -0500

    add function to get distance between points

--- main.py
+++ main.py
@@ -12,12 +12,8 @@ def solve_tsp(pts):
         [i, j] = sorted(random.sample(range(n), 2))
         new_tour = tour[:i] + tour[j:j + 1] + tour[i + 1:j] + tour[i:i + 1] + tour[j + 1:]
 
-        old_distance = sum(
-            [math.sqrt(sum([(pts[tour[(k + 1) % n]][d] - pts[tour[k % n]][d]) ** 2 for d in [0, 1]])) for k in
-             [j, j - 1, i, i - 1]])
-        new_distance = sum(
-            [math.sqrt(sum([(pts[new_tour[(k + 1) % n]][d] - pts[new_tour[k % n]][d]) ** 2 for d in [0, 1]])) for
-             k in [j, j - 1, i, i - 1]])
+        old_distance = sum([dist(pts[tour[(k + 1) % n]], pts[tour[k % n]]) for k in [j, j - 1, i, i - 1]])
+        new_distance = sum([dist(pts[new_tour[(k + 1) % n]], pts[new_tour[k % n]]) for k in [j, j - 1, i, i - 1]])
 
         if math.exp((old_distance - new_distance) / temp) > random.random():
             tour = copy.copy(new_tour)
@@ -32,6 +28,10 @@ def validate_tsp_input(pts):
     d = len(pts[0])
     for i in range(n):
         assert d == len(pts[i])
+
+
+def dist(p1, p2):
+    return math.sqrt(sum([(p1[e] - p2[e]) ** 2 for e in range(len(p1))]))
 
 
 cities = [random.sample(range(100), 2) for x in range(15)]

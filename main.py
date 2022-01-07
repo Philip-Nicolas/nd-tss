53343ceaf0a0a538739747b14f1164d0cf6120c3
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2022-01-07 13:51:16 -0500

    renamings for pep

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import random, numpy, math, copy, matplotlib.pyplot as plt
 
 
-## Adapted from https://ericphanson.com/blog/2016/the-traveling-salesman-and-10-lines-of-python/
+# Adapted from https://ericphanson.com/blog/2016/the-traveling-salesman-and-10-lines-of-python/
 
-def solveTsp(pts):
-    validateTspInput(pts)
+def solve_tsp(pts):
+    validate_tsp_input(pts)
     n = len(pts)
     tour = list(range(0, n))
 
     for temp in numpy.logspace(0, 5, num=100000)[::-1]:
         [i, j] = sorted(random.sample(range(n), 2))
-        newTour = tour[:i] + tour[j:j + 1] + tour[i + 1:j] + tour[i:i + 1] + tour[j + 1:]
+        new_tour = tour[:i] + tour[j:j + 1] + tour[i + 1:j] + tour[i:i + 1] + tour[j + 1:]
 
-        oldDistance = sum(
+        old_distance = sum(
             [math.sqrt(sum([(pts[tour[(k + 1) % n]][d] - pts[tour[k % n]][d]) ** 2 for d in [0, 1]])) for k in
              [j, j - 1, i, i - 1]])
-        newDistance = sum(
-            [math.sqrt(sum([(pts[newTour[(k + 1) % n]][d] - pts[newTour[k % n]][d]) ** 2 for d in [0, 1]])) for
+        new_distance = sum(
+            [math.sqrt(sum([(pts[new_tour[(k + 1) % n]][d] - pts[new_tour[k % n]][d]) ** 2 for d in [0, 1]])) for
              k in [j, j - 1, i, i - 1]])
 
-        if math.exp((oldDistance - newDistance) / temp) > random.random():
-            tour = copy.copy(newTour)
+        if math.exp((old_distance - new_distance) / temp) > random.random():
+            tour = copy.copy(new_tour)
 
     return tour
 
-def validateTspInput(pts):
+
+def validate_tsp_input(pts):
     n = len(pts)
     assert n > 1
 
@@ -34,7 +35,8 @@ def validateTspInput(pts):
 
 
 cities = [random.sample(range(100), 2) for x in range(15)]
-tour = solveTsp(cities)
+cities_tour = solve_tsp(cities)
 
-plt.plot([cities[tour[i % 15]][0] for i in range(16)], [cities[tour[i % 15]][1] for i in range(16)], 'xb-')
+plt.plot([cities[cities_tour[i % 15]][0] for i in range(16)], [cities[cities_tour[i % 15]][1] for i in range(16)],
+         'xb-')
 plt.show()
